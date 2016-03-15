9508f72167e8049973b0b3a1f517b52a4f72a868
darren.oleary <darren.oleary@solium.com>
2016-03-15 13:27:46 -0600

    Initial circular buffer implementation

---
+++ src/CircularBuffer.java
@@ -0,0 +1,38 @@
+public class CircularBuffer {
+
+    private Object[] _buffer;
+    private int readCursor = 0;
+    private int writeCursor = 0;
+
+    public CircularBuffer(int size) {
+	_buffer = new Object[size];
+    }
+
+    public boolean offer(Integer value) {
+        if (_buffer[writeCursor] != null) {
+            return false;
+        }
+        _buffer[writeCursor] = value;
+        writeCursor = next(writeCursor);
+
+	return true;
+    }
+
+    public Object poll() {
+        final Object value = _buffer[readCursor];
+
+        // should be optional
+        if (value != null) {
+            // once read we need to null out the buffer
+            _buffer[readCursor] = null;
+            readCursor = next(readCursor);
+        }
+
+        return value;
+    }
+
+    private int next(int index) {
+        // loop until to end of the buffer and loop around
+        return (index + 1) % _buffer.length;
+    }
+}
---
+++ test/unit/src/CircularBufferTest.java
@@ -0,0 +1,42 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CircularBufferTest {
+
+    private CircularBuffer buffer;
+
+    @Before
+    public void setup() {
+        buffer = new CircularBuffer(2);
+    }
+
+    @Test
+    public void poll_shouldPollElement_successfully() {
+
+        assertTrue(buffer.offer(1));
+
+        assertEquals(1, buffer.poll());
+        assertNull(buffer.poll());
+    }
+
+    @Test
+    public void offer_shouldNotOfferWhenBufferIsFull_successfully() {
+
+        assertTrue(buffer.offer(1));
+        assertTrue(buffer.offer(2));
+        assertFalse(buffer.offer(3));
+    }
+
+    @Test
+    public void poll_shouldRecycleBuffer_successfully() {
+
+        assertTrue(buffer.offer(1));
+        assertTrue(buffer.offer(2));
+        assertEquals(1, buffer.poll());
+        assertTrue(buffer.offer(3));
+        assertEquals(2, buffer.poll());
+        assertEquals(3, buffer.poll());
+    }
+}
